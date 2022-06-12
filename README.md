a295367f7f92f6c4f66b5c91a18bfbabef94aa72
bdeeesh <osama.emohsen@gmail.com>
2022-06-12 11:04:33 -0500

    added functions and info

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 set of functions to control scope RTP
 
+python3 oneRun.py -h
+
+set the options for a single run on the scope
+
+positional arguments:
+  rootname    root file name
+  frames      number of frames
+  run         run number
+  CH1_dV      Voltag per Division for CH1
+  CH2_dV      Voltag per Division for CH2
+  dt          Time per divison (H-SCALE)
+  RES         Resolution H-SCALE
+
 Example:
 
-python3 oneRun.py 0
+python3 oneRun.py rootname 20000 0 1e-3 100e-3 10e-9 50e-12
 
-will get a set of N frames for the defined values in oneRun.py and save the binary data to a local file
+#copying files
 
-''more info needed''
+Example: Change the file name inside copyOneFile.py
 
+python3 copyOneFile.py
+
+ 
+PyScopeAna.py contains functions to read and analyze data from the scope: 
+
+Example
+
+PyScopeAna.getTunefromScope(rootname,pi=0.41,pf=0.44,BaseLineRemove=True,integData=True,returnPOS=False)
+
+will look for the tune in the rootname file (Wfm data) with bin data between 0.41 and 0.44. The arguemtns will remove the BaseLine by averaging the data and/or return the integrated signal by using np.cumsum. 
+
+The output will be 4 arrays each with shape (N) (Number of frames x number of points):
+freq
+tune from CH1 without anything
+
+tune from CH1 with BaseLineRemoved
+
+tune from CH1 after integrating the data
 
 
--- oneRun.py
+++ oneRun.py
@@ -1,28 +1,60 @@
 import numpy as np
 
-from scopeFull import *
+from PyScope import *
 
 import time 
 import sys
 
-print (len(sys.argv))
 
-if len(sys.argv)>1:
-   m = sys.argv[1]
-else:
-   m = 0
+import argparse
 
 
-FILENAME = 'tuneVsGain'
-FRAMES = 20000
-#m = 0
-myScope = Control(FILENAME,FRAMES,LOG=1)
+
+parser = argparse.ArgumentParser(description='set the options for a single run on the scope') 
+
+
+parser.add_argument('rootname', type=str,
+                            help='root file name')
+
+parser.add_argument('frames', type=int,nargs='?',default=2000,
+                            help='number of frames')
+
+parser.add_argument('run', type=int,nargs='?',default=0,
+                            help='run number')
+parser.add_argument('CH1_dV', type=float,nargs='?',default=10e-3,
+                            help='Voltag per Division for CH1')
+
+parser.add_argument('CH2_dV', type=float,nargs='?',default=100e-3,
+                            help='Voltag per Division for CH2')
+
+parser.add_argument('dt', type=float,nargs='?',default=10e-9,
+                            help='Time per divison (H-SCALE)')
+
+parser.add_argument('RES', type=float,nargs='?',default=10e-9,
+                            help='Resolution H-SCALE')
+
+
+
+args = parser.parse_args()
+
+rootname =args.rootname
+FRAMES = args.frames
+m = args.run
+CH1_dV  =args.CH1_dV
+CH2_dV  =args.CH2_dV
+dt = args.dt
+RES = args.RES
+
+print (rootname,FRAMES,m,CH1_dV,CH2_dV)
+
+
+myScope = Control(rootname,FRAMES,LOG=1)
 
 
 myScope.preP(dtype='INT,8') # if the resolution is high, this will be ignored 
 
-myScope.setVscale(1,10e-3,0,0)
-myScope.setVscale(2,100e-3,0,0)
+myScope.setVscale(1,CH1_dV,0,0)
+myScope.setVscale(2,Ch2_dV,0,0)
 myScope.setHscale(10e-9,0,0)
 
 
